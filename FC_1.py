229ee34845a7a7788cda061dbb0b1e9a84d2ee85
XuyangAbert <53911455+XuyangAbert@users.noreply.github.com>
2023-07-24 19:08:59 -0500

    Update FC_1.py
    Eliminate unnecessary space between lines

--- FC_1.py
+++ FC_1.py
@@ -16,15 +16,11 @@ start = time.time()
 def Input():
     # Read the data from the txt file
     sample = pd.read_csv('GLIOMA.csv',header=None)
-    
     (N, L) = np.shape(sample)
-    
     dim = L - 1
-    
     label1 = sample.iloc[:,L-1]
     label = label1.values
     data = sample.iloc[:,0:dim]
-    
     # NewData = normalize(data)
     NewData = Pre_Data(data)
     OriData = data.values
@@ -39,52 +35,33 @@ def Pre_Data(data):
             NewData[:,i] = np.zeros((N,1))
         else:
             Temp = (Temp - np.min(Temp))/(max(Temp)-min(Temp))
-            NewData[:,i] = Temp
-            
+            NewData[:,i] = Temp    
     return NewData
                 
 def Distribution_Est(data, dim):
-    
     DC_mean = np.zeros(dim)
     DC_std = np.zeros(dim)
-    
     for i in range(dim):
-        
         TempClass = data[:,i]
-        
         DC_mean[i] = np.mean(TempClass)
-        DC_std[i] = np.std(TempClass)
-        
+        DC_std[i] = np.std(TempClass)  
     return DC_mean,DC_std
 
 def Feature_Dist(DC_means,DC_std,data,Var,dim,Corr):
-    
     DisC = np.zeros((dim,dim))
     Dist = []
     for i in range(dim):
-        
         for j in range(i,dim):
-            
             DisC[i,j] = KLD_Cal(data,i,j,Var,Corr)
             DisC[j,i] = DisC[i,j]
             Dist.append(DisC[i,j])
-    
     return DisC,Dist
 
 def KLD_Cal(data,i,j,Var,Corr):
-    
-    #Var1 = np.var(data[:,i])# Var[i]
-    #Var2 = np.var(data[:,j])# Var[j]
-    
     Var1 = Var[i]
     Var2 = Var[j]
-    
-    # P = np.corrcoef(data[:,i],data[:,j])[0,1]
     P = Corr[i,j]
-    
     Sim = Var1 + Var2 - ((Var1 + Var2)**2 - 4 * Var1 * Var2 * (1 - P**2))**0.5
-    
-    
     D_KL = Sim / (Var1 + Var2)
     
     return D_KL 
@@ -118,47 +95,32 @@ def Pseduo_Peaks(DisC, Dist, DC_Mean, DC_Std, data, fitness, StdF, gamma, Var):
     co = []
     F = fitness
     while True:
-        
         PeakIndices.append(np.argmax(F))
         Pfitness.append(np.max(F))
-    
         indices = NeighborSearch(DisC, data, sample, PeakIndices[i], marked, NeiRad, Var)
-        
         C_Indices[indices] = PeakIndices[i]
         if len(indices) == 0:
             indices=[PeakIndices[i]]
-        
         co.append(len(indices)) # Number of samples belong to the current 
-    # identified pseduo cluster
+        # identified pseduo cluster
         marked = np.concatenate(([marked,indices]))
-  
         # Fitness Proportionate Sharing
         F = Sharing(F, indices) 
-        
         # Check whether all of samples has been assigned a pseduo cluster label
         if np.sum(co) >= (len(F)):
-            
             break
-        
         i=i+1 # Expand the size of the pseduo cluster set by 1
     C_Indices = Close_FCluster(PeakIndices, DisC, np.shape(DisC)[0])
     return PeakIndices,Pfitness,C_Indices
 
 def NeighborSearch(DisC, data, sample, P_indice, marked, radius, Var):
     Cluster = []
-    
     for i in range(np.shape(sample)[0]):
-        
         if i not in marked:
-            
             Dist = DisC[i, P_indice]
-            
             if Dist <= radius:
-                Cluster.append(i)
-                
-                
+                Cluster.append(i)        
     Indices = Cluster
-    
     return Indices
 
 def Close_FCluster(FCluster,DisC,dim):
@@ -174,12 +136,10 @@ def Sharing(fitness, indices):
     for j in range(len(indices)):
         sum1 = sum1 + fitness[indices[j]]
     for th in range(len(indices)):
-            newfitness[indices[th]] = fitness[indices[th]] / (1+sum1)
-            
+            newfitness[indices[th]] = fitness[indices[th]] / (1+sum1)    
     return newfitness
     
 def Pseduo_Evolve(DisC, PeakIndices, PseDuoF, C_Indices, DC_Mean, DC_Std, data, fitness, StdF, gamma):
-    
     # Initialize the indices of Historical Pseduo Clusters and their fitness values
     HistCluster = PeakIndices
     HistClusterF = PseDuoF
@@ -189,10 +149,8 @@ def Pseduo_Evolve(DisC, PeakIndices, PseDuoF, C_Indices, DC_Mean, DC_Std, data, 
         # Check for the stablization of clutser evolution and exit the loop
         if len(np.unique(Cluster)) == len(np.unique(HistCluster)):
             break
-    
         # Update the feature indices of historical pseduo feature clusters and
         # their corresponding fitness values
-    
         HistCluster=Cluster
         HistClusterF=Cfitness
         C_Indices = F_Indices
@@ -259,7 +217,6 @@ def Pseduo_Merge(DisC, PeakIndices, PseDuoF, C_Indices, DC_Mean, DC_Std, data, f
     for n in range(len(PeakIndices)):
         if PeakIndices[n] in Unmarked:
             NewPI.append(PeakIndices[n])
-
     # Updated pseduo feature clusters information after merging
     FCluster = np.unique(NewPI)
     Ffitness = fitness[FCluster]
@@ -267,70 +224,41 @@ def Pseduo_Merge(DisC, PeakIndices, PseDuoF, C_Indices, DC_Mean, DC_Std, data, f
     return FCluster, Ffitness, F_Indices
 
 def Boundary_Points(DisC, F_Indices, data, Current, Neighbor):
-    
     [N, dim] = np.shape(data)
     TempCluster1 = np.where(F_Indices == Current)
     TempCluster2 = np.where(F_Indices == Neighbor)
-    
     TempCluster = np.append(TempCluster1,TempCluster2)
-    D = []
-#    D = np.inf
-#    FI = Current
-#    print(len(TempCluster))
     for i in range(len(TempCluster)):
         D1 = DisC[TempCluster[i], Current]
         D2 = DisC[TempCluster[i], Neighbor]
-#        if D < abs(D1-D2):
-#            D = abs(D1-D2)
-#            FI = i
         D.append(abs(D1 - D2))
     if not D:
         BD = Current
     else:
         FI = np.argmin(D)
         BD = TempCluster[FI]
-    
     return BD
 
 def PseduoGeneration(PseP,N):
-    
     Pse_Mean = PseP[:,0]
     Pse_Std = PseP[:,1]
-    
-    # Data = (np.zeros((N,len(Pse_Mean))))
-    
     Data = np.zeros((N,len(Pse_Mean)))
-    
     for i in range(len(Pse_Mean)):
-        
         Data[:, i] = (np.repeat(Pse_Mean[i],N) + Pse_Std[i] * np.random.randn(N)).T
-    
     return Data
 
 def Psefitness_cal( PseP, sample, data, PseduoData, StdF, gamma):
-    
     OriFN = np.shape(sample)[0]
     PN = np.shape(PseP)[0]
     PsePF = np.zeros(PN)
-    
-    
     for i in range(PN):
-        
         TempSum = 0
-        
         for j in range(OriFN):
-            
             Var1 = np.var(data[:,j])
             Var2 = np.var(PseduoData[:,i])
-            
-            
-            
             P = np.corrcoef(data[:,j],PseduoData[:,i])[0,1]
-            
             Sim = Var1 + Var2 - ((Var1 + Var2)**2 - 4 * Var1 * Var2 * (1 - P**2))**0.5
-            
             D_KL = Sim / (Var1 + Var2)
-            
             TempSum = TempSum + (math.exp(-(D_KL**2)/StdF))**gamma
         PsePF[i] = TempSum
     return PsePF
@@ -339,54 +267,29 @@ def Psefitness_cal( PseP, sample, data, PseduoData, StdF, gamma):
 if __name__ == '__main__':
     [data,label, OriData] = Input()   
     [N, dim] = np.shape(data)
-
-    [DC_means, DC_std] = Distribution_Est(data,dim)  
-
+    [DC_means, DC_std] = Distribution_Est(data,dim) 
     Var = np.var(data,axis=0)
-    
     Corr = np.corrcoef(data.T)
-
     DisC,Dist =  Feature_Dist(DC_means,DC_std,data,Var,dim,Corr)
-    
     end1 = time.time()
     print('Distance Calculation Finished:',end1-start)
-    #Dist = []
-    
-#    for i in range(len(DC_means)):
-#        for j in range(len(DC_means)):
-#            if j != i:
-#                Dist.append(DisC[i,j])
     StdF = (np.max(np.power(Dist,0.5)))**2
-    # StdF = np.var(Dist)
-
     gamma = 5
-
     fitness = fitness_cal(DisC, DC_means, DC_std, data, StdF, gamma)
-    
     oldfitness = np.copy(fitness)
-    
-
-    [PeakIndices,Pfitness,C_Indices] = Pseduo_Peaks(DisC, Dist, DC_means,DC_std,data,fitness,StdF,gamma, Var)
-    
+    [PeakIndices,Pfitness,C_Indices] = Pseduo_Peaks(DisC, Dist, DC_means,
+                                                    DC_std,data,fitness,StdF,gamma, Var)
     fitness = oldfitness
-
-     
     # Pseduo Clusters Infomormation Extraction
     PseDuo = DC_means[PeakIndices] # Pseduo Feature Cluster centers
     PseDuoF = Pfitness # Pseduo Feature Clusters fitness values
     #-------------Check for possible merges among pseduo clusters-----------#
-    
     [FCluster,Ffitness,C_Indices] = Pseduo_Evolve(DisC, PeakIndices, PseDuoF, C_Indices, DC_means, DC_std, data, fitness, StdF, gamma)
-
     SF = FCluster
-
     Extract_FIndices = SF
-    
     label = label.reshape(N,1)
-    
     Extract_Data = np.concatenate((OriData[:,SF],label),axis=1)
     np.savetxt('Extract_GLIOMA2.txt',Extract_Data)
-#    np.savetxt('Extract_Pro.txt',Extract_Data)
     end2 = time.time()
     print('The total time in seconds:',end2-start)
     
